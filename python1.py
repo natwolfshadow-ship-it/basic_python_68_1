0a1bb4dd881e4c0af9e6e743a9a3f05d91adb002
naphat <natwolfshadow@gmail.com>
2026-01-31 13:19:14 +0700

    Python Vaiables

---
+++ python1.py
@@ -0,0 +1,41 @@
+"""
+#
+# Part: Python Comment
+#
+"""
+print("Hello,World!")
+
+"""
+#
+# Part: Python variables
+#
+"""
+x = 10
+y = 20
+print(x,y)
+
+x = str(5)
+y = int("5")
+z = float("5.0")
+print(type(x), type(y), type(z))
+
+"""
+#
+# Part: Variable Naming 
+#
+"""
+myver = 100
+my_ver = 200
+_my_ver = 300 
+myVer = 400
+MYVER = 500
+# my-ver = 600  # Invalid variable name
+# 2myver = 700  # Invalid variable name
+# my ver = 800  # Invalid variable name
+
+#Camel Case
+myVariableName = "900"
+#Pascal Case
+MyVariableName = "1000"
+#Snake Case
+my_variable_name = "1100"

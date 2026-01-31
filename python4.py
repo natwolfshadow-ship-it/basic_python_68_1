c908e177768bbad49ceb9c98add18a1c9aa73be5
naphat <natwolfshadow@gmail.com>
2026-01-31 14:25:06 +0700

    Conditional Statements

---
+++ python4.py
@@ -0,0 +1,32 @@
+"""
+#
+# Conditional Statements in Python
+#
+"""
+a = 10
+b = 20
+if a < b:
+    print("a is less than b")
+elif a == b:
+    print("a is equal to b")
+elif a <= b:
+    print("a is less than or equal to b")
+else:
+    print("a is greater than b")
+
+if a < b:
+    print("a is less than b")
+if a > b:
+    print("a is greater than b")
+
+if a < b: 
+    print("a is less than b")  # Single line if statement
+if a == b: 
+    print("a is equal to b")  # Single line if statement
+
+if a < b: 
+    print("a is less than b")  # Single line if statement
+else:
+    print("a is not less than b")  # Single line else statement
+
+    

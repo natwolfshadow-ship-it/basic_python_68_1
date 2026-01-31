7a750d04e5188e96370696e17a4b578cf1ce430a
naphat <natwolfshadow@gmail.com>
2026-01-31 14:12:44 +0700

    Collection of Python

---
+++ python3.py
@@ -0,0 +1,53 @@
+"""
+#
+# Part: Collection of Python Operators
+# Lists, Tuples, Sets, and Dictionaries
+#
+"""
+
+"""
+#
+# Lists
+#
+"""
+this_list = ["apple", "banana", "cherry"]
+print(this_list)
+print(this_list[1])  # Accessing first element
+print(type(this_list)) # Checking type
+print(len(this_list)) # Length of the list
+
+"""
+#
+# Tuples
+#
+"""
+this_tuple = ("apple", "banana", "cherry")
+print(this_tuple)
+print(this_tuple[1])  # Accessing first element
+print(type(this_tuple)) # Checking type
+print(len(this_tuple)) # Length of the tuple
+
+"""
+#
+# Sets
+#
+"""
+this_set = {"apple", "banana", "cherry"}
+print(this_set)
+print(type(this_set)) # Checking type
+print(len(this_set)) # Length of the set
+
+"""
+#
+# Dictionaries
+#
+"""
+this_dict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(this_dict)
+print(this_dict["brand"])
+print(type(this_dict)) # Checking type
+print(len(this_dict)) # Length of the dictionary

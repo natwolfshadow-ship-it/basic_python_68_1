c092bbc52af2de7fd1822a4d4742361b20973130
naphat <natwolfshadow@gmail.com>
2026-01-31 13:57:45 +0700

    Operators

---
+++ python2.py
@@ -0,0 +1,55 @@
+"""
+#
+#Part: Python Operators
+#Arithmetic Operators
+#
+"""
+x = 4 + 5 # Addition
+y = 10 - 2 # Subtraction
+z = 3 * 7 # Multiplication
+a = 20 / 4# Division
+b = 10 % 3# Modulus
+c = 2 ** 3# Exponentiation
+
+"""
+#
+#Part: Python Operators
+#Assignment Operators
+#
+"""
+x = 5
+x += 3  # Equivalent to x = x + 3
+x = x + 3 # Equivalent to x = 3
+
+"""
+#
+#Part: Python Operators
+#comparison Operators
+#
+"""
+a = 5
+b = 10 
+print(a == b)  # Equal to
+print(a != b)  # Not equal to
+print(a > b)   # Greater than
+print(a < b)   # Less than
+print(a >= b)  # Greater than or equal to
+print(a <= b)  # Less than or equal to
+
+"""
+#
+#Part: Python Operators
+#logical Operators
+#
+"""
+x = True
+y = False
+print(x and y)  # Logical AND
+print(x or y)   # Logical OR
+print(not x)    # Logical NOT
+print(x & y)    # Logical NOT
+print(x | y)    # Logical OR
+print(~x)       # Logical NOT
+a = 5
+b = 10
+print((a < 10) and (b > 5))  # True
